f7047ea3d8d4898a58749e504c72fd2b5df40457
Du Park <dupark3@gmail.com>
2018-03-05 10:24:54 -0500

    Modified to read, sort, and output pass/fail info only

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <algorithm> // max, sort
-#include <iomanip> // streamsize, setprecision
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,9 +14,7 @@ int main()
     // read using member function read and store the data as class objects into vector students
     while(record.read(std::cin)) {
         maxLen = std::max(maxLen, record.name().size());
-        if (record.valid()){
-            record.grade();
-            students.push_back(record);
+        students.push_back(record);
     }
 
     // alphabetize students by name
@@ -25,12 +22,9 @@ int main()
 
     // write the names and grades of students
     for (std::vector<Student_info>::size_type i = 0; i != students.size(); ++i){
-        std::streamsize prec = std::cout.precision();
         std::cout << students[i].name() << std::string(maxLen + 1 - students[i].name().size(), ' ')
-                  << std::setprecision(3) << students[i].showFinalGrade()
-                  << std::setprecision(prec) << std::endl;
+                  << students[i].showPassOrFail() << std::endl;
     }
-
 
     return 0;
 }
